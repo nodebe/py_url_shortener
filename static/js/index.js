52b075730e524ea3eec16a4a7e03dcc793a76b12
nodebe <nodywelete3@gmail.com>
2018-11-06 14:11:31 +0100

    added some jquery and ajax to post json to backend

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,32 @@
-$document.ready(function(){
-    
-})
+$(document).ready(function(){
+    $('#error').hide()
+    $('#submit').click(function(){
+        var title = $('#title').val()
+        var url = $('#long_url').val()
+        url_info = {'values':[title,url]}
+        if ((title && url) != ''){
+            if ((title.length > 2) && (title.length < 20 )){
+                if (url.length > 15) {
+                    var dir = '/shorten'
+                    var server = 'http://127.0.0.1:5000'
+                    $.ajax({
+                        type: "POST",
+                        url:server+dir,
+                        data: JSON.stringify(url_info),
+                        data_type: 'json'
+                    }).done(function(result){
+                        console.log(result)
+                    }) 
+                }else{
+                    $('#error').text("Url must be higher than 15 characters long!");
+                    $('#error').show(300);
+                }  
+            }else{
+                $('#error').text("Title must be between 3 and 20 characters long!");
+                $('#error').show(300);
+            }
+        }else{
+            $('#error').show(300);
+        }
+    });
+});
